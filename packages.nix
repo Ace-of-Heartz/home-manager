35eb9b6e7dab6b93b769e32f52ae08fcd988ac33
Ferenci Ákos Sándor <synthetace@gmail.com>
2024-08-06 23:56:46 +0200

    Migrated packages

--- packages.nix
+++ packages.nix
@@ -2,7 +2,168 @@
 
 {
   home.packages = with pkgs; [
+
+    # Debug: 
     hello
 
+    # Network & Browsing:
+    floorp
+    cifs-utils
+    networkmanager
+    samba
+    samba4Full
+    nfs-utils
+    nas
+    
+    # Office Tools:
+    libreoffice-still-unwrapped
+
+    # Torrent / File Sharing:
+    nicotine-plus
+
+    # Terminals & Shells:
+    rio
+    kitty
+    fish 
+
+    # File Management:
+    krusader
+
+    # Misc. Terminal Extensions:
+    kittysay
+    kitty-img
+    kitty-themes
+    neofetch
+
+    # System Management:
+    gparted 
+
+    # Project Tools:
+    gitkraken
+
+    # Package Tools:
+    appimage-run
+    
+    # Socials:
+    discord
+    vesktop
+    telegram-desktop
+    teams-for-linux
+
+    # Modelling & CAD:
+    blender-hip
+    blockbench
+
+    # Art Tools:
+    krita
+    aseprite
+    pureref
+
+    # Graphics Tools:
+    mandelbulber
+    renderdoc
+    clinfo
+    lact
+
+    # Media Players:
+    spotify 
+    vlc
+    strawberry
+
+    # Game Dev.:
+    unityhub
+    godot_4
+
+    # Games & Launchers:
+    prismlauncher
+    steam 
+    #itch
+
+    # Wacom & Drawing Tablets:
+    xf86_input_wacom
+    libwacom
+
+    # E-Book & Comics
+    calibre
+    comical
+
+    # Note Taking:
+    obsidian
+
+
+    # Virtual Machines & Tools for Virtualization:
+    virt-manager
+    qemu
+    qemu_kvm
+    qemu-utils
+    qtemu
+    quickemu
+    libvirt
+
+    # Misc. Software:
+    ripgrep
+    fd
+    unp
+    unrar
+    grub2
+    pandoc
+    flameshot
+    socat
+    aha 
+    xwayland
+    waylandpp
+    wayland-utils
+    wayland-scanner
+    krename
+    kget
+    findutils
+    plocate
+    diffutils
+    
+    anki
+    bitwarden-desktop
+    
+    # VM:
+    qemu
+    qemu_kvm
+    qemu-utils
+    qtemu
+    quickemu
+    libvirt
+
+    # Icons:
+    fairywren
+
+    # Wine & Other Dangerous Substances
+    wine64
+    bottles
+
+    # IDEs # Editors:
+    jetbrains-toolbox
+
+    emacs
+    emacsPackages.kurecolor
+    emacsPackages.rgb
+    vscodium
+    kdePackages.kate
+      
+    # Dev. Tools.:
+    shellcheck
+    riff
+
+    # Fonts:
+    nerdfonts
+    julia-mono
+    fira-code
+    jetbrains-mono
+    overpass
+    comic-relief
+    maple-mono-NF
+
+    # Print & scan: 
+    epsonscan2
+    epson-escpr2
+    libinklevel
+    foomatic-db
   ];
 }
